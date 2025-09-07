ddf2d7c4d77cdc5b98cd3516dd3174a4004405fd
charlesrobison <crobison@uw.edu>
2025-09-07 19:37:51 -0400

    Add task and sub flow

--- hello.py
+++ hello.py
@@ -1,7 +1,20 @@
-from prefect import flow
+from prefect import flow, task
+
+@task
+def create_message():
+    msg = "Hello from task!"
+    return msg
+
+@flow
+def something_else():
+    result = 10
+    return result
 
 @flow
 def hello_world():
-    print("Hello, world!")
+    sub_flow_message = something_else()
+    task_message = create_message()
+    new_message = task_message + str(sub_flow_message)
+    print(new_message) 
 
 hello_world()
